29f9f74087a8600ffdc1052dbfaddaccc519bbf0
Naresh.Kumar <naresh.kumar@valetliving.com>
2021-03-11 10:41:32 +0530

    removed unused decorator

--- src/weather_service/urls.py
+++ src/weather_service/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-from weather_service.decorators import protected
 
 
 urlpatterns = [
